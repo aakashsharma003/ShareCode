62c289d0adae92cd3cf5fb42df3d8dafbd361efe
Akash Sharma <aakash6263264@gmail.com>
2024-07-27 19:24:46 +0530

    resolved path error

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ const io = new Server(server);
 
 // ---------------------Deployment-----------------------------------------------
 // const __dirname = path.resolve();
-app.use(express.static("../frontend/dist"));
+// app.use(express.static("../frontend/dist"));
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname,'..','frontend', 'dist', 'index.html'));
-})
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname,'..','frontend', 'dist', 'index.html'));
+// })
 
 
 // app.get("*", (req, res) => {
